196538a54ac666c64c678e147780728ac729b83c
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-08-30 14:24:45 +0300

    fixup! fixup! fixup! fixup! fixup! Cleanup

--- iosApp/iosApp/Components/ScheduleTableSmallHeader.swift
+++ iosApp/iosApp/Components/ScheduleTableSmallHeader.swift
@@ -6,10 +6,25 @@ class ScheduleTableSmallHeader : UITableViewHeaderFooterView {
     func displayDay(title: String) {
         self.title.text = title
         self.title.textColor = UIColor.dayGray
+//        animate()
     }
 
     func displayLunch(title: String) {
         self.title.text = title
         self.title.textColor = UIColor.redOrange
+
+//        animate()
+    }
+
+
+    private func animate() {
+        UIView.transition(with: title, duration: 10.0, options: [.curveLinear], animations: {
+            self.title.frame.origin.x -= 100
+        }) { _ in
+            self.animate()
+            print("before", self.title.frame.origin.x)
+            self.title.frame.origin.x = self.frame.origin.x
+        }
+
     }
 }
